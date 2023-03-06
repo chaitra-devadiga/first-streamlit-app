c64e525ca1ab7f7ebe7edd0336839d47d07ccd2b
chaitra-devadiga <127088361+chaitra-devadiga@users.noreply.github.com>
2023-03-06 14:20:49 +0530

    Update streamlit_app.py
    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('🥣 Breakfast Favourites')
-streamlit.text('🥗 Omega 3 and Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach, & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocoado Toast')
